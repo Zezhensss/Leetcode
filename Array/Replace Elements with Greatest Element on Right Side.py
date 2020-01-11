e971e2a10c8a75e3d7f8e7766e1aeb1006f8fe38
ZBirdy <520125843@qq.com>
2020-01-11 23:11:40 +0000

    3

---
+++ Array/Replace Elements with Greatest Element on Right Side.py
@@ -0,0 +1,38 @@
+# Given an array arr, replace every element in that array with the greatest element among the elements to its right, and replace the last element with -1.
+
+# After doing so, return the array.
+
+ 
+
+# Example 1:
+
+# Input: arr = [17,18,5,4,6,1]
+# Output: [18,6,6,6,1,-1]
+ 
+
+# Constraints:
+
+# 1 <= arr.length <= 10^4
+# 1 <= arr[i] <= 10^5
+
+class Solution:
+    def replaceElements(self, arr: List[int]) -> List[int]:
+        for i in range(len(arr)-1):
+            if i != 0 and arr[i]<arr[i-1]:
+                arr[i] = arr[i-1]
+            else:
+                arr[i] = max(arr[i+1:])
+        arr[-1] = -1
+        return arr
+
+# doublej's answer (which is faster)
+# class Solution:
+#     def replaceElements(self, arr: List[int]) -> List[int]:
+#         if not len(arr): return []
+#         large = float('-inf')
+#         for i in range(len(arr)-1,-1,-1):
+#             elem = arr[i]
+#             arr[i] = large
+#             large = max(elem, large)
+#         arr[-1] = -1
+#         return arr
